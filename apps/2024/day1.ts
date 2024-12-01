134c8f8f2927a8a0cc2f9173f0fd80e1427ab9ab
Giampiero Sciarra <g.sciarra@outlook.com>
2024-12-01 19:43:01 +0100

    feat(2024): day1 cleaning

--- apps/2024/day1.ts
+++ apps/2024/day1.ts
@@ -1,5 +1,4 @@
 import { config } from "dotenv";
-import { resolve } from "path";
 import { getParsedInput } from "@repo/utils/src";
 
 config(); // Load .env file
@@ -86,67 +85,3 @@ async function solveDay1part2() {
 
 solveDay1part1();
 solveDay1part2();
-//
-// const filePath = resolve(__dirname, "./input/day1.txt");
-//
-// const rawInput = readFileSync(filePath, "utf-8");
-// const lines = rawInput.split("\n");
-// let input = [];
-//
-// for (let line of lines) {
-//   if (line === "") continue;
-//
-//   const [a, b] = line.split(/\s+/).map((num) => parseInt(num, 10));
-//   input.push([a, b]);
-// }
-//
-//
-// const leftList = input.map(([a]) => a).sort(
-//   (a, b) => a - b
-// );
-// const rightList = input.map(([_, b]) => b).sort(
-//   (a, b) => a - b
-// );
-// Part 1 solution
-// let totalDistance = 0;
-
-// for (let index = 0; index < leftList.length; index++) {
-//   const left = leftList[index];
-//   const right = rightList[index];
-//
-//   if (left === right) {
-//     continue;
-//   }
-//
-//   totalDistance += Math.abs(right - left);
-// }
-
-// console.log("part1:",totalDistance);
-
-// Part 2 solution
-// const repetitions: Map<number, number> = new Map();
-// let similarityScore = 0;
-//
-// const getRepetitions = (n: number) => {
-//   if (repetitions.has(n)) {
-//     return repetitions.get(n) as number;
-//   }
-//
-//   const repeated = rightList.reduce((acc, right) => {
-//     if (right === n) {
-//       acc++;
-//     }
-//
-//     return acc;
-//   }, 0)
-//   repetitions.set(n, repeated);
-//   return repeated;
-// }
-//
-// for (let index = 0; index < leftList.length; index++) {
-//   const left = leftList[index];
-//   const repeated = getRepetitions(left);
-//   similarityScore += (repeated * left);
-// }
-//
-// console.log("part2:", similarityScore);
